3dae01e8eb6217679050eb24f8d2bc7941b48dd7
SornchaiTheDev <sornchaithedev@gmail.com>
2024-06-21 00:45:10 +0700

    fix: change service account firebase file from json to env

--- src/app/api/firebase.ts
+++ src/app/api/firebase.ts
@@ -1,7 +1,10 @@
 import admin from "firebase-admin";
-import serviceAccount from "./service_account.json";
 import { ServiceAccount, getApps, initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
+
+const serviceAccount = process.env.SERVICE_ACCOUNT;
+
+if(serviceAccount === undefined) throw new Error("Service Account not found please provide it in .env")
 
 const apps = getApps();
 
